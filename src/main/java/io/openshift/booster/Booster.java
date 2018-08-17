f067b37a857717f8b36ba3cf7b7e59561933216c
Kiran Krishnamurthy <kiran.krishnamurthy@live.com>
2018-08-17 02:33:00 +0000

    master: updated changes

--- src/main/java/io/openshift/booster/Booster.java
+++ src/main/java/io/openshift/booster/Booster.java
@@ -1,4 +1,24 @@
 package io.openshift.booster;                                                      
-public class Booster {                                                      
- public static void main(String[] args) { }                                                      
-} 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@SpringBootApplication
+public class Booster {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Booster.class);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> monoRouterFunction(EchoHandler echoHandler) {
+		return route(POST("/echo"), echoHandler::echo);
+	}
+
+}
